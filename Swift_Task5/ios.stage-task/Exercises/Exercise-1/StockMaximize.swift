7db22370da684ed8a874b4dd353ff94667eb0fbe
Dzmitry Navitski <spyofmossad@gmail.com>
2021-06-13 11:16:46 +0300

    feat: add exercise1 and exercise2 impl

--- Swift_Task5/ios.stage-task/Exercises/Exercise-1/StockMaximize.swift
+++ Swift_Task5/ios.stage-task/Exercises/Exercise-1/StockMaximize.swift
@@ -3,6 +3,29 @@ import Foundation
 class StockMaximize {
 
     func countProfit(prices: [Int]) -> Int {
-        return -1
+        
+        var maxPrice = 0
+        var profit = 0
+        var bottomIndex = 0
+        
+        if prices.count == 1 { return profit }
+        
+        for (index, price) in prices.enumerated() {
+            if price >= maxPrice {
+                maxPrice = price
+                
+                if index < prices.count - 1 {
+                    continue
+                }
+            }
+                        
+            for n in bottomIndex...index - 1 {
+                profit += maxPrice - prices[n]
+            }
+            bottomIndex = index
+            maxPrice = 0
+        }
+        
+        return profit
     }
 }
--- Swift_Task5/ios.stage-task/Exercises/Exercise-2/MessageDecryptor.swift
+++ Swift_Task5/ios.stage-task/Exercises/Exercise-2/MessageDecryptor.swift
@@ -3,6 +3,53 @@ import UIKit
 class MessageDecryptor: NSObject {
     
     func decryptMessage(_ message: String) -> String {
-        return ""
+        
+        var input = message
+        var output: String = ""
+        
+        while input.count > 0 {
+            var item = String(input.prefix(while: {$0 != "["}))
+            if let multiplier = Int(item) {
+                let firstRange = input.range(of: item)
+                input = input.replacingCharacters(in: firstRange!, with: "")
+                
+                let subMessage = getSubMessage(of: input)
+                let decryprted = decryptMessage(subMessage)
+                
+                output.append(String(repeating: decryprted, count: multiplier))
+                
+                let secondRange = input.range(of: subMessage)
+                input = input.replacingCharacters(in: secondRange!, with: "")
+            } else {
+                item = String(input.first!)
+                if item != "]" && item != "[" {
+                    output.append(item)
+                }
+                input = String(input.dropFirst())
+            }
+        }
+        
+        return output
+    }
+    
+    private func getSubMessage(of message: String) -> String {
+        
+        var subMessage = ""
+        var counter = 0
+        
+        for char in message {
+            subMessage.append(char)
+            if char != "]" {
+                if char == "[" {
+                    counter += 1
+                }
+            } else {
+                counter -= 1
+                if counter == 0 {
+                    break
+                }
+            }
+        }
+        return subMessage
     }
 }
